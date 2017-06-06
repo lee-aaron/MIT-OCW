f88bb9ea0b49d44b92f9457a22307b3073508ee9
lee-aaron <sphinxer34@yahoo.com>
2017-06-06 11:27:11 -0700

    Another lesson

---
+++ Python/user_input.py
@@ -0,0 +1,6 @@
+print "raw_input returns a string"
+raw = raw_input("Enter a string: ")
+print type(raw)
+print "input preserves type (used for int or float)"
+a = input("Enter a number: ")
+print type(a)
